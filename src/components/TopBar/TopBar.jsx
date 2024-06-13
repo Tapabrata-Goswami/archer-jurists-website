d55fa4a434c901ead5b95c518bb6d1c402bca3ef
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2024-06-13 22:20:30 +0530

    Topbar & navbar components adding

---
+++ src/components/TopBar/TopBar.jsx
@@ -0,0 +1,11 @@
+import style from './TobBar.module.css';
+
+function TopBar(){
+    return(
+        <div className={`container-fluid ${style.TopBarMainContainer}`}>
+            
+        </div>
+    );
+}
+
+export default TopBar;
